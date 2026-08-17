732b3418c5b994ee6d8b31d00a25c906bcd079a9
kranthivarma12 <kranthivarma222@gmail.com>
2026-08-17 11:38:01 +0530

    Fix duplicate product migration

--- database/migrations/2026_08_17_054655_create_products_table.php
+++ database/migrations/2026_08_17_054655_create_products_table.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name');
+            $table->unsignedInteger('quantity_in_stock');
+            $table->decimal('price_per_item', 12, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
